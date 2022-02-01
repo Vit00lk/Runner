adee675865e5f2260e6f0f204845dcedf0a83cb1
unknown <all4os@yandex.ru>
2022-02-01 19:12:11 +0400

    first load project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void CreateObjects()
+    {
+        Instantiate(objects, new Vector3(12.35f, 3.69f, 0), Quaternion.identity);
+    }
+}
---
+++ Assets/ObjectMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ObjectMove : MonoBehaviour
+{
+    public float s;
+
+    private void Start()
+    {
+        s = 0.16f;
+    }
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x - s, transform.position.y, 0);
+        if (transform.position.x <= -27.5f)
+
+        {
+
+            //Destroy(gameObject);-1.30712
+
+        }
+    }
+}
